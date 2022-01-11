c47a301e4fc9bc9703db18c355364cba9ca3f958
Thắng Nguyễn Võ Đức <19522213@ms.uit.edu.vn>
2022-01-12 01:14:51 +0700

    Addd back-end into account screen

---
+++ src/main/java/project/javafx_fixed_asset_management/Models/ACCOUNT.java
@@ -0,0 +1,31 @@
+package project.javafx_fixed_asset_management.Models;
+
+public class ACCOUNT {
+    String email;
+    String password;
+    String accountId;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+}
