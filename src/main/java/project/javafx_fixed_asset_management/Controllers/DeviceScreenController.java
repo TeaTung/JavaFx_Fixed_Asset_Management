049a6c8aed4a2d9e66a63c7bd3a447c4c6482071
Thắng Nguyễn Võ Đức <19522213@ms.uit.edu.vn>
2022-01-13 14:13:47 +0700

    Fix conflict

--- src/main/java/project/javafx_fixed_asset_management/Controllers/DeviceScreenController.java
+++ src/main/java/project/javafx_fixed_asset_management/Controllers/DeviceScreenController.java
@@ -1,258 +1,180 @@
 package project.javafx_fixed_asset_management.Controllers;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import jfxtras.styles.jmetro.FlatAlert;
+
+
 import jfxtras.styles.jmetro.JMetro;
-import jfxtras.styles.jmetro.JMetroStyleClass;
+
+import jfxtras.styles.jmetro.MDL2IconFont;
 import jfxtras.styles.jmetro.Style;
+import project.javafx_fixed_asset_management.Controllers.AccountScreenController.AccountDialogController;
 import project.javafx_fixed_asset_management.Main;
-import project.javafx_fixed_asset_management.Models.DATABASE_DAO;
-import project.javafx_fixed_asset_management.Models.DEVICE;
-import project.javafx_fixed_asset_management.Models.DEVICE_ADD;
-import project.javafx_fixed_asset_management.Models.UNIT;
-import project.javafx_fixed_asset_management.Utils.Utils;
 
 import java.io.IOException;
 import java.net.URL;
-import java.time.LocalDate;
-import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.function.Predicate;
+import java.util.Stack;
 
+public class ManagerHomeScreenController implements Initializable {
+    int userId = 1;
 
-public class DeviceScreenController implements Initializable {
+    public void openDeviceScreenButtonAction(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Views/DeviceScreen/device_screen.fxml"));
 
-    public ObservableList<DEVICE> devicesList;
-    @FXML
-    public TableView<DEVICE> deviceTableView;
-    //
-    @FXML
-    public TableColumn<DEVICE, String> deviceNameColumn;
-    @FXML
-    public TableColumn<DEVICE, String> idColumn;
-    @FXML
-    public TableColumn<DEVICE, String> typeOfDeviceColumn;
-    @FXML
-    public TableColumn<DEVICE, Integer> priceColumn;
-    @FXML
-    public TableColumn<UNIT, String> unitColumn;
-    @FXML
-    public TableColumn<DEVICE, String> specificationColumn;
-    @FXML
-
-    public TableColumn<DEVICE, String> dateOfManufacturingColumn;
-    @FXML
-    public TableColumn<DEVICE, LocalDate> dateOfUsingColumn;
-    @FXML
-    public TableColumn<DEVICE, String> statusColumn;
-    @FXML
-    public TableColumn<DEVICE, Float> percentDamage;
-    public TableColumn<DEVICE, Integer> quantityColumn;
-    public Button ExportBtn;
-    public Button addBtn;
-    public Button deleteBtn;
-    public Button updateBtn;
-    public Button backBtn;
-    @FXML
-    public TextField searchTF;
-    public AnchorPane anchorPane;
-    public ComboBox<String> deviceViewModeCB;
-    public TableColumn<DEVICE, String> departmentNameColumn;
-    FilteredList<DEVICE> filteredList;
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        new Thread(() -> {
-
-            // TABLEVIEW
-            var devices = new DATABASE_DAO<>(DEVICE_ADD.class);
-            devicesList = FXCollections.observableArrayList(devices.selectList(
-                    "select * " +
-                            "from tbDevice inner join tbDeviceModel " +
-                            "on tbDevice.ModelId =  tbDeviceModel.ModelId " +
-                            "inner join tbUnit \n" +
-                            "on tbDeviceModel.UnitId = tbUnit.UnitId"));
-
-
-            deviceNameColumn.setCellValueFactory(
-                    new PropertyValueFactory<DEVICE, String>("deviceName"));
-
-            idColumn.setCellValueFactory(
-                    new PropertyValueFactory<DEVICE, String>("deviceId"));
-
-            typeOfDeviceColumn.setCellValueFactory(
-                    new PropertyValueFactory<>("modelName"));
-
-            priceColumn.setCellValueFactory(
-                    new PropertyValueFactory<>("price"));
-
-            unitColumn.setCellValueFactory(
-                    new PropertyValueFactory<UNIT, String>("unitName"));
-
-
-            specificationColumn.setCellValueFactory(
-                    new PropertyValueFactory<DEVICE, String>("specification"));
-
-            dateOfManufacturingColumn.setCellValueFactory(
-                    new PropertyValueFactory<>("yearManufacture"));
-
-
-            dateOfUsingColumn.setCellValueFactory(
-                    new PropertyValueFactory<>("yearUsed"));
-
-
-            statusColumn.setCellValueFactory(
-                    new PropertyValueFactory<>("deviceStatus"));
-
-            quantityColumn.setCellValueFactory(
-                    new PropertyValueFactory<>("quantityDevice"));
-
-            percentDamage.setCellValueFactory(new PropertyValueFactory<>("percentDamage"));
-            filteredList = new FilteredList<>(devicesList);
-            deviceTableView.setItems(devicesList);
-
-            // VIEWMODECB
-            deviceViewModeCB.getItems().add("TOTAL");
-            deviceViewModeCB.getItems().add("IN USE");
-
-
-        }).start();
-
-
-        anchorPane.getStyleClass().add(JMetroStyleClass.BACKGROUND);
-        deviceTableView.getStyleClass().add(JMetroStyleClass.TABLE_GRID_LINES);
-        deviceViewModeCB.setValue("TOTAL");
-
-    }
-
-
-    public void addDeviceButtonAction(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Views/DeviceScreen/AddNewDeviceDialog/add_new_device_dialog.fxml"));
         Node node = (Node) event.getSource();
-        Stage stage = new Stage();
-        stage.initModality(Modality.APPLICATION_MODAL);
-        Scene scene = new Scene(fxmlLoader.load(), 990, 650);
-
-
-        stage.setTitle("Add New Screen Dialog");
-        stage.setScene(scene);
-        JMetro jMetro = new JMetro(Style.LIGHT);
-        jMetro.setScene(scene);
-        stage.showAndWait();
-        updateData();
-
-    }
-
-    public void updateDeviceButtonAction(ActionEvent event) throws IOException {
-
-        if (deviceTableView.getSelectionModel().getSelectedItem() != null) {
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Views/DeviceScreen/UpdateNewDeviceDialog/update_device_dialog.fxml"));
-
-            Node node = (Node) event.getSource();
-
-            Stage stage = new Stage();
-
-            // CREATE THE SCENE
-            Parent root = fxmlLoader.load();
-            Scene scene = (new Scene(root, 1018, 660));
-            stage.setScene(scene);
+        Stage stage = (Stage) node.getScene().getWindow();
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), 1280, 720);
             JMetro jMetro = new JMetro(Style.LIGHT);
             jMetro.setScene(scene);
 
-            //Set the Stage
-            UpdateScreenDialogController newProjectController = fxmlLoader.getController();
-            stage.initModality(Modality.APPLICATION_MODAL);
-            stage.setTitle("Update Device Dialog");
-
-
-            // CREATE DEVICE_ADD OBJECT
-            int cellIndex = deviceTableView.getSelectionModel().getSelectedIndex();
-            var device = deviceTableView.getSelectionModel().getSelectedItem();
-            var quantityDevice = quantityColumn.getCellData(cellIndex);
-            var deviceAdd = new DEVICE_ADD(quantityDevice, device);
-
-            newProjectController.setDEVICE_ADD(deviceAdd);
-            stage.showAndWait();
-            updateData();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        stage.setScene(scene);
+        JMetro jMetro = new JMetro(Style.LIGHT);
+        jMetro.setScene(scene);
+        stage.show();
     }
 
-    public void updateData() {
-
-        var devices = new DATABASE_DAO<>(DEVICE_ADD.class);
-        devicesList = FXCollections.observableArrayList(devices.selectList(
-                "select * " +
-                        "from tbDevice inner join tbDeviceModel " +
-                        "on tbDevice.ModelId =  tbDeviceModel.ModelId " +
-                        "inner join tbUnit \n" +
-                        "on tbDeviceModel.UnitId = tbUnit.UnitId"));
-        filteredList = new FilteredList<>(devicesList);
-        deviceTableView.setItems(devicesList);
-
-    }
-
-    public void deleteDeviceButtonAction(ActionEvent event) {
-        if (deviceTableView.getSelectionModel().getSelectedItem() != null) {
-            FlatAlert alert = new FlatAlert(Alert.AlertType.CONFIRMATION);
-            alert.setContentText("Are you sure ?");
-
-            JMetro jMetro = new JMetro(Style.LIGHT);
-            jMetro.setScene(alert.getDialogPane().getScene());
-            Optional<ButtonType> result = alert.showAndWait();
-            if (result.get() == ButtonType.OK) {
-                new DATABASE_DAO<>(DEVICE.class).delete("DELETE FROM tbDevice where deviceId = ? ", deviceTableView.getSelectionModel().getSelectedItem().getDeviceId());
-                updateData();
-            } else {
-            }
-        }
-    }
-
-    public void onKeyTypeSearchTF(KeyEvent keyEvent) {
-
-        String textSearch = searchTF.getText();
-        if (!textSearch.isEmpty()) {
-
-
-            filteredList.setPredicate(
-                    new Predicate<DEVICE>() {
-                        public boolean test(DEVICE t) {
-                            return (t.getDeviceName().toLowerCase().startsWith(textSearch));
-                        }
-                    }
-            );
-
-            deviceTableView.setItems(filteredList);
-
-
-        } else {
-            deviceTableView.setItems(devicesList);
-        }
-    }
-
-    public void exportButtonAction(ActionEvent event) throws IOException {
+    public void openInventoryScreenButtonAction(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Views/InventoryScreen/first_inventory_screen.fxml"));
         Node node = (Node) event.getSource();
         Stage stage = (Stage) node.getScene().getWindow();
-        Utils.exportExcelDevices(stage, deviceTableView);
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), 1280, 720);
+            JMetro jMetro = new JMetro(Style.LIGHT);
+            jMetro.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.setScene(scene);
+
+        stage.show();
     }
 
-    public void backDeviceButtonAction(ActionEvent event) {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Views/HomeScreen/Manager/manager_home_screen.fxml"));
+    public void openLiquidationScreenButtonAction(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Views/LiquidationScreen/first_liquidation_screen.fxml"));
         Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), 1280, 720);
+            JMetro jMetro = new JMetro(Style.LIGHT);
+            jMetro.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void openTransferScreenButtonAction(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Views/TransferScreen/transfer_devices_screen.fxml"));
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), 1280, 720);
+            JMetro jMetro = new JMetro(Style.LIGHT);
+            jMetro.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.setScene(scene);
+
+        stage.show();
+    }
+
+    public void openAccountScreenButtonAction(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Views/AccountScreen/account_screen.fxml"));
+        Node node = (Node) event.getSource();
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        Parent accountDialogController = fxmlLoader.load();
+        Scene scene = new Scene(accountDialogController);
+        AccountDialogController controller = fxmlLoader.getController();
+        controller.init(userId);
+
+        JMetro jMetro = new JMetro(Style.LIGHT);
+        jMetro.setScene(scene);
+
+        stage.setTitle("Profile");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void openRepairScreenButtonAction(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Views/RepairScreen/repair_devices_screen.fxml"));
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), 1280, 720);
+            JMetro jMetro = new JMetro(Style.LIGHT);
+            jMetro.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.setScene(scene);
+
+        stage.show();
+    }
+
+    public void openHistoryAndManagementScreenButtonAction(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Views/HistoryAndManagementScreen/history_management_screen.fxml"));
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), 1280, 740);
+            scene.setFill(Color.TRANSPARENT);
+            JMetro jMetro = new JMetro(Style.LIGHT);
+            jMetro.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void openConfigScreenButtonAction(MouseEvent MouseEvent) {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Views/ConfigScreen/config_screen.fxml"));
+        Node node = (Node) MouseEvent.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), 600, 520);
+            JMetro jMetro = new JMetro(Style.LIGHT);
+            jMetro.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    public void openLiqdsd(MouseEvent mouseEvent) {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Views/LiquidationScreen/first_liquidation_screen.fxml"));
+        Node node = (Node) mouseEvent.getSource();
         Stage stage = (Stage) node.getScene().getWindow();
         Scene scene = null;
         try {
@@ -264,41 +186,6 @@ public class DeviceScreenController implements Initializable {
         JMetro jMetro = new JMetro(Style.LIGHT);
         jMetro.setScene(scene);
         stage.show();
-    }
 
-    public void deviceViewModeCbOnAction(ActionEvent actionEvent) {
-        switch (deviceViewModeCB.getSelectionModel().getSelectedItem().toString()) {
-            case "IN USE": {
-                departmentNameColumn.setVisible(true);
-                var devices = new DATABASE_DAO<>(DEVICE_ADD.class);
-                devicesList = FXCollections.observableArrayList(devices.selectList(
-                        "select  *  " +
-                                "from tbDevice inner join tbDeviceModel " +
-                                "on tbDevice.ModelId =  tbDeviceModel.ModelId " +
-                                "inner join tbUnit " +
-                                "on tbDeviceModel.UnitId = tbUnit.UnitId " +
-                                "inner join tbTransfer on tbTransfer.deviceId = tbDevice.deviceId"));
-                departmentNameColumn.setCellValueFactory(
-                        new PropertyValueFactory<>("department"));
-                deviceTableView.setItems(devicesList);
-                break;
-            }
-
-            case "TOTAL": {
-                var devices = new DATABASE_DAO<>(DEVICE_ADD.class);
-                devicesList = FXCollections.observableArrayList(devices.selectList(
-                        "select * " +
-                                "from tbDevice inner join tbDeviceModel " +
-                                "on tbDevice.ModelId =  tbDeviceModel.ModelId " +
-                                "inner join tbUnit " +
-                                "on tbDeviceModel.UnitId = tbUnit.UnitId "));
-                departmentNameColumn.setVisible(false);
-                departmentNameColumn.setCellValueFactory(
-                        new PropertyValueFactory<>("department"));
-                deviceTableView.setItems(devicesList);
-                System.out.println("TOTAL");
-                break;
-            }
-        }
     }
 }
